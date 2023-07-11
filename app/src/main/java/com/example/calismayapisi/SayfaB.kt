2b79004a4ad12fe90efb4e67f5d719aa27269f24
esragg <esragarcia@hotmail.com>
2023-07-11 17:12:19 +0200

    SayfaB'de geri tusu ile aktiviteyi kapatma

--- app/src/main/java/com/example/calismayapisi/SayfaB.kt
+++ app/src/main/java/com/example/calismayapisi/SayfaB.kt
@@ -1,5 +1,8 @@
 package com.example.calismayapisi
 
+import android.app.Activity
+import android.util.Log
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -7,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
@@ -20,4 +24,10 @@ fun SayfaB() {
     ){
         Text(text = "Sayfa B", fontSize = 50.sp)
     }
+
+    val activity = (LocalContext.current as Activity)
+    BackHandler(onBack = { //artik geri tusuna bastigimizda ne istersen onu yapacak
+        Log.e("SayfaB","Geri Tusuna Basildi")
+        activity.finish() //Programi kapat (geri tusu ile)
+    })
 }
